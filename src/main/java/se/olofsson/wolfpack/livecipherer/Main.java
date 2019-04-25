d9d16229beaedc38bd2d5c70b394fe982adeafec
Christoffer Olofsson <15955091+ChrisAcrobat@users.noreply.github.com>
2019-04-25 22:03:32 +0200

    Get generated version number from Gradle.

--- src/main/java/se/olofsson/wolfpack/livecipherer/Main.java
+++ src/main/java/se/olofsson/wolfpack/livecipherer/Main.java
@@ -1,6 +1,9 @@
 package se.olofsson.wolfpack.livecipherer;
 
 import javax.swing.*;
+import java.net.URL;
+import java.util.jar.Attributes;
+import java.util.jar.Manifest;
 
 /**
  * Created on 2016-05-29.
@@ -8,7 +11,7 @@ import javax.swing.*;
  */
 public class Main
 {
-    public static final String CURRENT_VERSION = "v0.3";    // TODO: Get from Gradle.
+    public static final String CURRENT_VERSION = getCurrentVersion();
 
     public static void main(String[] args)
     {
@@ -19,5 +22,23 @@ public class Main
         }
 
         new LiveCipher().setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+    }
+
+    private static String getCurrentVersion(){
+        // Source (modified): https://stackoverflow.com/a/1273432
+        String currentVersion = null;
+        try{
+            Class clazz = Main.class;
+            String className = clazz.getSimpleName() + ".class";
+            String classPath = clazz.getResource(className).toString();
+            if (!classPath.startsWith("jar")){
+                return null;
+            }
+            String manifestPath = classPath.substring(0, classPath.lastIndexOf("!") + 1) + "/META-INF/MANIFEST.MF";
+            Manifest manifest = new Manifest(new URL(manifestPath).openStream());
+            Attributes attr = manifest.getMainAttributes();
+            currentVersion = attr.getValue("LiveCipherer-Version");
+        }catch(Exception e){}
+        return currentVersion;
     }
 }
